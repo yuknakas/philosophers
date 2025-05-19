70dd1470b0a2b165c38a5dc3dcf8654593c446e0
Yuki Nakashiba <yukinakashiba@gmail.com>
2025-05-19 17:05:52 +0900

    initial commit

---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuknakas <yuknakas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 15:40:28 by yuknakas          #+#    #+#             */
+/*   Updated: 2025/05/19 15:45:33 by yuknakas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+//libraries to be included
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
